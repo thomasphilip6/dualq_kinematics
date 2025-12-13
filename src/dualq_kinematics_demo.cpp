c81273ccc0182144ab1a4e129418a7a44c684e46
Thomas Philip <lethomasphilip@gmail.com>
2025-12-13 17:58:35 +0100

    feat: add working version of demo

--- src/dualq_kinematics_demo.cpp
+++ src/dualq_kinematics_demo.cpp
@@ -19,18 +19,57 @@
 
 //link of the robot considered the tip of the kinematic chain
 const std::string c_pandaTipLink = "panda_link8";
+
+//Tolerance of match between MoveIt Forward Kinematics and Dual Quaternion FK
+constexpr double c_tolerance = 1e-6;
 using DualQuaternion = dualq_kinematics::DualQuaternion<double>;
 using ScrewCoordinates = dualq_kinematics::ScrewCoordinates<double>;
 const std::string ROBOT_DESCRIPTION_PARAM = "robot_description";
+static const rclcpp::Logger LOGGER = rclcpp::get_logger("dualq_kinematics_demo");
 
 //Demo showing forward kinematics with dual quaternions and screw theory on a MoveIt configured robot
 
-void dualQuaternionForwardKinematics(std::vector<DualQuaternion>& p_screwDQ, DualQuaternion& p_tip2BaseInit, std::vector<double>& p_jointValuesRad, Eigen::Isometry3d& p_tip2BaseComputed)
+/**
+ * @brief Prints Screw Coordinates information: axes, positions and tip2BaseInit
+ * @param p_screwCoord ScrewCoordinates instance to be printed
+ */
+void printScrewInfo(ScrewCoordinates& p_screwCoord)
 {
-    DualQuaternion l_tip2BaseDQComputed = (p_screwDQ.at(0)*p_jointValuesRad.at(0)).dqExp();
-    for (size_t i = 1; i < p_jointValuesRad.size(); i++)
+    for (auto &&l_axis : p_screwCoord.getScrewAxes())
     {
-        l_tip2BaseDQComputed = l_tip2BaseDQComputed * (p_screwDQ.at(i)*p_jointValuesRad.at(i)).dqExp();
+        RCLCPP_INFO_STREAM(LOGGER, "Screw Axis :  [" << l_axis.x() << ", " << l_axis.y() << ", " << l_axis.z() << " ]");
+    }
+
+    for (auto &&l_pos : p_screwCoord.getPositions())
+    {
+        RCLCPP_INFO_STREAM(LOGGER, "Position :  [" << l_pos.x() << ", " << l_pos.y() << ", " << l_pos.z() << " ]");
+    }
+    RCLCPP_INFO_STREAM(LOGGER, "Tip2BaseInit :  [" << p_screwCoord.getTip2BaseInit().matrix() << " ]");
+}
+
+/**
+ * @brief Prints Eigen Isometry information as translation and rotation
+ * @param p_screwCoord Eigen::Isometry instance to be printed
+ */
+void printEigenIsometry(const Eigen::Isometry3d& p_transformationMatrix)
+{
+    RCLCPP_INFO_STREAM(LOGGER, "Translation: \n" << p_transformationMatrix.translation() << "\n");
+    RCLCPP_INFO_STREAM(LOGGER, "Rotation: \n" << p_transformationMatrix.rotation() << "\n");
+}
+
+/**
+ * @brief Computes the forward kinematics using dual quaternions
+ * @param p_screwDQ ScrewCoordinates of the robot
+ * @param p_tip2BaseInit Initial tip2Base transform (robot at rest)
+ * @param p_jointValues_rad Joint Values to be applied to the joints
+ * @param p_tip2BaseComputed The result of the forward kinematics computation as an Eigen Isometry
+ */
+void dualQuaternionForwardKinematics(const std::vector<DualQuaternion>& p_screwDQ, const DualQuaternion& p_tip2BaseInit, std::vector<double>& p_jointValues_rad, Eigen::Isometry3d& p_tip2BaseComputed)
+{
+    DualQuaternion l_tip2BaseDQComputed = (p_screwDQ.at(0) * p_jointValues_rad.at(0) * 0.5).dqExp();
+    for (size_t i = 1; i < p_jointValues_rad.size(); i++)
+    {
+        l_tip2BaseDQComputed = l_tip2BaseDQComputed * (p_screwDQ.at(i)*p_jointValues_rad.at(i) * 0.5).dqExp();
     }
     l_tip2BaseDQComputed = l_tip2BaseDQComputed * p_tip2BaseInit;
     p_tip2BaseComputed = l_tip2BaseDQComputed.getTransform();    
@@ -43,7 +82,6 @@ int main(int argc, char** argv)
         "dualq_kinematics_demo",
         rclcpp::NodeOptions().automatically_declare_parameters_from_overrides(true)
     );
-    const auto& LOGGER = node->get_logger();
 
     //Loading robot and constructing screw coordinates
     rdf_loader::RDFLoader rdf_loader(node, ROBOT_DESCRIPTION_PARAM);
@@ -55,12 +93,13 @@ int main(int argc, char** argv)
     auto l_stop = std::chrono::high_resolution_clock::now();
     std::chrono::duration<double, std::milli> l_ms = l_stop - l_start;
     RCLCPP_INFO_STREAM(LOGGER, "Screw Coordinates constructed in " << l_ms.count() << " ms");
+    //printScrewInfo(l_screwCoord);
 
     //Constructing Dual Quaternions from Screw Coordinates
     RCLCPP_INFO_STREAM(LOGGER, "Computing " << l_screwCoord.getJointsNames().size()+1 << " dual quaternions representing the screw coordinates");
     l_start = std::chrono::high_resolution_clock::now();
     std::vector<DualQuaternion> l_screwDQ;
-    for (size_t i = 0; i < l_screwDQ.size(); i++)
+    for (size_t i = 0; i < l_screwCoord.getJointsNames().size(); i++)
     {
         l_screwDQ.push_back(DualQuaternion( l_screwCoord.getScrewAxes().at(i), l_screwCoord.getPositions().at(i) ));
     }
@@ -71,30 +110,44 @@ int main(int argc, char** argv)
 
     //Constructing Robot State
     moveit::core::RobotStatePtr l_robotState(new moveit::core::RobotState(l_robotModel));
+    l_robotState->enforceBounds();
     l_robotState->setToDefaultValues();
     const moveit::core::JointModelGroup* l_jointModelGroup = l_robotModel->getJointModelGroup("panda_arm");
     const std::vector<std::string>& l_jointNames = l_jointModelGroup->getVariableNames();
 
+    //First Try, apply joint values, compute and compare FK
+    std::vector<double> l_jointValuesReady_rad ={0, -0.785, 0, -2.356, 0, 1.571, 0.785};
+    l_robotState->setJointGroupPositions(l_jointModelGroup, l_jointValuesReady_rad);
     std::vector<double> l_jointValues_rad;
-
-    l_robotState->enforceBounds();
-    l_robotState->setToRandomPositions(l_jointModelGroup);
     l_robotState->copyJointGroupPositions(l_jointModelGroup, l_jointValues_rad);
-    for (std::size_t i = 0; i < l_jointNames.size(); ++i)
-    {
-        RCLCPP_INFO(LOGGER, "Joint %s: %f", l_jointNames[i].c_str(), l_jointValues_rad[i]);
-    }
     const Eigen::Isometry3d& l_eeStateMoveIt = l_robotState->getGlobalLinkTransform("panda_link8");
-    RCLCPP_INFO_STREAM(LOGGER, "Translation from MoveIt: \n" << l_eeStateMoveIt.translation() << "\n");
-    RCLCPP_INFO_STREAM(LOGGER, "Rotation from MoveIt: \n" << l_eeStateMoveIt.rotation() << "\n");
+    l_stop = std::chrono::high_resolution_clock::now();
+    RCLCPP_INFO(LOGGER, "MoveIt FK Returned : ");
+    printEigenIsometry(l_eeStateMoveIt);
+
     Eigen::Isometry3d l_eeStateDQ;
     l_start = std::chrono::high_resolution_clock::now();
     dualQuaternionForwardKinematics(l_screwDQ, l_tip2BaseInit, l_jointValues_rad, l_eeStateDQ);
     l_stop = std::chrono::high_resolution_clock::now();
     l_ms = l_stop - l_start;
-    RCLCPP_INFO_STREAM(LOGGER, "Dual Quaternions FK took " << l_ms.count() << " ms");
-    RCLCPP_INFO_STREAM(LOGGER, "Translation from DQ FK: \n" << l_eeStateDQ.translation() << "\n");
-    RCLCPP_INFO_STREAM(LOGGER, "Rotation from DQ FK: \n" << l_eeStateDQ.rotation() << "\n");
+    RCLCPP_INFO_STREAM(LOGGER, "Dual Quaternions FK took " << l_ms.count() << " ms. And result is: ");
+    printEigenIsometry(l_eeStateDQ);
+    RCLCPP_INFO_STREAM(LOGGER, "Dual Quaternions FK and MoveIt FK match: " << l_eeStateDQ.isApprox(l_eeStateMoveIt, c_tolerance));
+
+    //Second Try to make sure twists were not damaged
+    l_robotState->setToRandomPositions(l_jointModelGroup);
+    l_robotState->copyJointGroupPositions(l_jointModelGroup, l_jointValues_rad);
+    const Eigen::Isometry3d& l_eeStateMoveIt2 = l_robotState->getGlobalLinkTransform("panda_link8");
+    RCLCPP_INFO(LOGGER, "MoveIt FK Returned : ");
+    printEigenIsometry(l_eeStateMoveIt2);
+
+    l_start = std::chrono::high_resolution_clock::now();
+    dualQuaternionForwardKinematics(l_screwDQ, l_tip2BaseInit, l_jointValues_rad, l_eeStateDQ);
+    l_stop = std::chrono::high_resolution_clock::now();
+    l_ms = l_stop - l_start;
+    RCLCPP_INFO_STREAM(LOGGER, "Dual Quaternions FK took " << l_ms.count() << " ms. And result is: ");
+    printEigenIsometry(l_eeStateDQ);
+    RCLCPP_INFO_STREAM(LOGGER, "Dual Quaternions FK and MoveIt FK match: " << l_eeStateDQ.isApprox(l_eeStateMoveIt2, c_tolerance));
 
     rclcpp::shutdown();
     return 0; 
