a53560f0895f079de0630331bfcc38f03a96ce2f
Thomas Philip <lethomasphilip@gmail.com>
2026-01-23 20:13:30 +0100

    test: add Paden Kahan 2nd sub-problem test

--- test/PadenKahanTest.cpp
+++ test/PadenKahanTest.cpp
@@ -5,6 +5,7 @@
 
 constexpr double c_tolerance = 1e-3;
 using FirstPadenKahan = dualq_kinematics::FirstPadenKahanProblem<double>;
+using SecondPadenKahan = dualq_kinematics::SecondPadenKahanProblem<double>;
 
 TEST(dualq_kinematics, FirstPadenKahanProblemTest)
 {
@@ -36,6 +37,39 @@ TEST(dualq_kinematics, FirstPadenKahanProblemTest)
 
     FirstPadenKahan l_testResultInfinite2(l_positionOnLine, l_jointScrewAxis, l_endPoint, l_endPoint);
     EXPECT_FALSE(l_testResultInfinite2.getResult().has_value()) << "Input with start=end with Paden Kahan 1st Problem shouldn't return a finite number";
+}
+
+TEST(dualq_kinematics, SecondPadenKahanProblemTest)
+{
+    //panda robot dimentions
+    const double l_d1 = 0.333;
+    const double l_d3 = 0.316;
+    const double l_d5 = 0.384;
+    //const double l_a5 = 0.0825;
+    const double l_df = 0.107;
+    const double l_a7 = 0.088;
+
+    Eigen::Quaternion l_joint1ScrewAxis(0.0, 0.0, 0.0, 1.0);
+    Eigen::Quaternion l_joint2ScrewAxis(0.0, 0.0, 1.0, 0.0);
+
+    Eigen::Vector3d l_intersectionPoint;
+    Eigen::Vector3d l_startPoint;
+    Eigen::Vector3d l_endPoint;
+    l_intersectionPoint << 0.0, 0.0, l_d1;
+    l_startPoint << l_a7, 0.0, l_d1+l_d3+l_d5-l_df;
+    l_endPoint << -0.151662, -0.0553609, 0.910345;
+
+    SecondPadenKahan l_testResultFinite(l_intersectionPoint, l_joint1ScrewAxis, l_joint2ScrewAxis, l_startPoint, l_endPoint);
+
+    EXPECT_EQ(l_testResultFinite.getAngle1Result().size(), 1);
+    EXPECT_EQ(l_testResultFinite.getAngle2Result().size(), 1);
+    std::cout << "number of angle pairs " << l_testResultFinite.getAngle1Result().size() << std::endl;
+
+    for (size_t i = 0; i < l_testResultFinite.getAngle1Result().size(); i++)
+    {
+        std::cout << "Angle 1 : " << l_testResultFinite.getAngle1Result().at(i) << " rad" << std::endl;
+        std::cout << "Angle 2 : " << l_testResultFinite.getAngle2Result().at(i) << " rad" << std::endl;
+    }
 }
 
 int main(int argc, char ** argv)
