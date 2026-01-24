cbe5a1c4db8d66722a11e3c1b81f73abd1ed58d7
Thomas Philip <lethomasphilip@gmail.com>
2026-01-24 14:53:50 +0100

    test: improve Paden Kahan tests

--- test/PadenKahanTest.cpp
+++ test/PadenKahanTest.cpp
@@ -17,21 +17,17 @@ TEST(dualq_kinematics, FirstPadenKahanProblemTest)
     const double l_df = 0.107;
     const double l_a7 = 0.088;
 
-    Eigen::Quaternion l_jointScrewAxis(0.0, 0.0, 0.0, 1.0);
-    Eigen::Vector3d l_positionOnLine;
-    Eigen::Vector3d l_startPoint;
-    Eigen::Vector3d l_endPoint;
-    l_positionOnLine << 0.0, 0.0, l_d1;
-    l_startPoint << l_a7, 0.0, l_d1+l_d3+l_d5-l_df;
-    l_endPoint << 0.0621879, 0.0622628, 0.926;
+    Eigen::Quaterniond l_jointScrewAxis(0.0, 0.0, 0.0, 1.0);
+    Eigen::Quaterniond l_positionOnLine(0.0, 0.0, 0.0, l_d1);
+    Eigen::Quaterniond l_startPoint(0.0, l_a7, 0.0, l_d1+l_d3+l_d5-l_df);
+    Eigen::Quaterniond l_endPoint(0.0, 0.0621879, 0.0622628, 0.926);
 
     FirstPadenKahan l_testResultFinite(l_positionOnLine,  l_jointScrewAxis, l_startPoint, l_endPoint);
     EXPECT_TRUE(l_testResultFinite.getResult().has_value()) << "Input on Paden Kahan 1st Problem should return a finite number";
     EXPECT_TRUE(FirstPadenKahan::compareFloatNum(0.786, l_testResultFinite.getResult().value(), c_tolerance)) << "Result of Paden Kahan Problem is Wrong";
     std::cout << "Result computed " << l_testResultFinite.getResult().value() << " rad" << std::endl;
 
-    Eigen::Vector3d l_startPointOnLine;
-    l_startPointOnLine << 0.0, 0.0, l_d1+l_d3+l_d5-l_df;
+    Eigen::Quaterniond l_startPointOnLine(0.0, 0.0, 0.0, l_d1+l_d3+l_d5-l_df);
     FirstPadenKahan l_testResultInfinite1(l_positionOnLine, l_jointScrewAxis, l_startPointOnLine, l_endPoint);
     EXPECT_FALSE(l_testResultInfinite1.getResult().has_value()) << "Input with start on axis with Paden Kahan 1st Problem shouldn't return a finite number";
 
@@ -49,26 +45,28 @@ TEST(dualq_kinematics, SecondPadenKahanProblemTest)
     const double l_df = 0.107;
     const double l_a7 = 0.088;
 
-    Eigen::Quaternion l_joint1ScrewAxis(0.0, 0.0, 0.0, 1.0);
-    Eigen::Quaternion l_joint2ScrewAxis(0.0, 0.0, 1.0, 0.0);
+    const std::vector<double> l_expectedSigma1 = {0.35, -2.79159};
+    const std::vector<double> l_expectedSigma2 = {-0.42, 0.125352};
 
-    Eigen::Vector3d l_intersectionPoint;
-    Eigen::Vector3d l_startPoint;
-    Eigen::Vector3d l_endPoint;
-    l_intersectionPoint << 0.0, 0.0, l_d1;
-    l_startPoint << l_a7, 0.0, l_d1+l_d3+l_d5-l_df;
-    l_endPoint << -0.151662, -0.0553609, 0.910345;
+    Eigen::Quaterniond l_joint1ScrewAxis(0.0, 0.0, 0.0, 1.0);
+    Eigen::Quaterniond l_joint2ScrewAxis(0.0, 0.0, 1.0, 0.0);
+
+    Eigen::Quaterniond l_startPoint(0.0, l_a7, 0.0, l_d1+l_d3+l_d5-l_df);
+    Eigen::Quaterniond l_endPoint(0.0, -0.151662, -0.0553609, 0.910345);
+    Eigen::Quaterniond l_intersectionPoint(0.0, 0.0, 0.0, l_d1);
 
     SecondPadenKahan l_testResultFinite(l_intersectionPoint, l_joint1ScrewAxis, l_joint2ScrewAxis, l_startPoint, l_endPoint);
 
-    EXPECT_EQ(l_testResultFinite.getAngle1Result().size(), 1);
-    EXPECT_EQ(l_testResultFinite.getAngle2Result().size(), 1);
+    EXPECT_EQ(l_testResultFinite.getAngle1Result().size(), l_expectedSigma1.size());
+    EXPECT_EQ(l_testResultFinite.getAngle2Result().size(), l_expectedSigma1.size());
     std::cout << "number of angle pairs " << l_testResultFinite.getAngle1Result().size() << std::endl;
 
     for (size_t i = 0; i < l_testResultFinite.getAngle1Result().size(); i++)
     {
         std::cout << "Angle 1 : " << l_testResultFinite.getAngle1Result().at(i) << " rad" << std::endl;
+        EXPECT_TRUE(FirstPadenKahan::compareFloatNum(l_expectedSigma1.at(i), l_testResultFinite.getAngle1Result().at(i), 0.01));
         std::cout << "Angle 2 : " << l_testResultFinite.getAngle2Result().at(i) << " rad" << std::endl;
+        EXPECT_TRUE(FirstPadenKahan::compareFloatNum(l_expectedSigma2.at(i), l_testResultFinite.getAngle2Result().at(i), 0.01));
     }
 }
 
