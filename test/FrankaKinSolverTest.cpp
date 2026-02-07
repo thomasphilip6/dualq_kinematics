23a7ceb10461f6cea5c5a3ae4958f22476ca79b5
Thomas Philip <lethomasphilip@gmail.com>
2026-02-07 17:11:31 +0100

    test: add FrankaKinSolver test

---
+++ test/FrankaKinSolverTest.cpp
@@ -0,0 +1,93 @@
+#include <gtest/gtest.h>
+#include "dualq_kinematics/DualQuaternion.h"
+#include "dualq_kinematics/ScrewCoordinates.h"
+#include "dualq_kinematics/FrankaKinSolver.h"
+
+//ROS2
+#include <pluginlib/class_loader.hpp>
+#include <rclcpp/rclcpp.hpp>
+
+// MoveIt
+#include <moveit/kinematics_base/kinematics_base.h>
+#include <moveit/rdf_loader/rdf_loader.h>
+#include <moveit/robot_model_loader/robot_model_loader.h>
+
+#include <moveit/utils/robot_model_test_utils.h>
+
+//C++
+#include <map>
+#include <string>
+#include <memory>
+
+const std::string c_pandaTipLink = "panda_link8";
+const size_t c_repetitions = 1000;
+constexpr double c_tolerance = 1e-6;
+
+using ScrewCoordinates = dualq_kinematics::ScrewCoordinates<double>;
+using FrankaKinSolver = dualq_kinematics::FrankaKinSolver<double>;
+using Vector3 = Eigen::Matrix<double, 3, 1>; 
+
+const std::string ROBOT_DESCRIPTION_PARAM = "robot_description";
+static const rclcpp::Logger LOGGER = rclcpp::get_logger("FrankaKinSolverTest");
+
+TEST(dualq_kinematics, FrankaKinSolverTest)
+{
+    auto const node = std::make_shared<rclcpp::Node>(
+        "FrankaKinSolver_test",
+        rclcpp::NodeOptions().automatically_declare_parameters_from_overrides(true)
+    );
+
+    RCLCPP_INFO_STREAM(LOGGER, "Loading robot model from " << node->get_name() << "." << ROBOT_DESCRIPTION_PARAM);
+    std::cout << "Retrieving Robot Model" << std::endl;
+    rdf_loader::RDFLoader rdf_loader(node, ROBOT_DESCRIPTION_PARAM);
+    moveit::core::RobotModelPtr l_robotModel = std::make_shared<moveit::core::RobotModel>(rdf_loader.getURDF(), rdf_loader.getSRDF());
+    ASSERT_TRUE(bool(l_robotModel)) << "Failed to load robot model";
+
+    ScrewCoordinates l_screwCoord(*l_robotModel, c_pandaTipLink);
+
+    FrankaKinSolver l_frankaKin(l_screwCoord);
+
+    //Constructing Robot State
+    moveit::core::RobotStatePtr l_robotState(new moveit::core::RobotState(l_robotModel));
+    l_robotState->enforceBounds();
+    l_robotState->setToDefaultValues();
+    const moveit::core::JointModelGroup* l_jointModelGroup = l_robotModel->getJointModelGroup("panda_arm");
+
+    // ------------ FK Test ---------------- //
+    std::vector<double> l_jointValuesReady_rad ={0.14, -0.785, 0, -2.356, 0, 1.571, 0.785};
+    l_robotState->setJointGroupPositions(l_jointModelGroup, l_jointValuesReady_rad);
+    Eigen::Isometry3d l_computedResult;
+    l_frankaKin.computeTipFK(l_jointValuesReady_rad, l_computedResult);
+    const Eigen::Isometry3d& l_eeStateMoveIt1 = l_robotState->getGlobalLinkTransform("panda_link8");
+    EXPECT_TRUE(l_eeStateMoveIt1.isApprox(l_computedResult, c_tolerance)) << "FK fails";
+
+    for (size_t i = 0; i < c_repetitions; i++)
+    {
+        l_robotState->setToRandomPositions();
+        std::vector<double> l_jointValues;
+        l_robotState->copyJointGroupPositions(l_jointModelGroup, l_jointValues);
+        l_frankaKin.computeTipFK(l_jointValues, l_computedResult);
+        const Eigen::Isometry3d& l_eeStateMoveIt = l_robotState->getGlobalLinkTransform("panda_link8");
+        EXPECT_TRUE(l_eeStateMoveIt.isApprox(l_computedResult, c_tolerance)) << "FK fails";
+    }
+
+    // ------------ computeWristPosition Test ---------------- //
+    
+    l_jointValuesReady_rad ={0.14, -0.785, 0, -2.356, 0, 1.571, 0.0};
+    l_robotState->setJointGroupPositions(l_jointModelGroup, l_jointValuesReady_rad);
+    const Eigen::Isometry3d& l_eeWanted = l_robotState->getGlobalLinkTransform("panda_link8");
+    const Eigen::Isometry3d& l_wristExpected = l_robotState->getGlobalLinkTransform("panda_link6");
+    Vector3 l_wrist;
+    l_frankaKin.computeWristPosition(l_eeWanted, l_jointValuesReady_rad.at(6), l_wrist);
+
+    EXPECT_TRUE(l_wristExpected.translation().isApprox(l_wrist, c_tolerance)) << "Compute Wrist fails";
+
+}
+
+int main(int argc, char ** argv)
+{
+    testing::InitGoogleTest(&argc, argv);
+    rclcpp::init(argc, argv);
+    int result = RUN_ALL_TESTS();
+    return result;
+}
---
+++ test/launch/FrankaKinSolver.test.py
@@ -0,0 +1,59 @@
+import unittest
+import launch_testing
+import pytest
+
+import launch
+import launch_ros
+import launch_testing.actions
+from launch_testing.util import KeepAliveProc
+import rclpy
+
+from moveit_configs_utils import MoveItConfigsBuilder
+from launch_param_builder import ParameterBuilder
+
+@pytest.mark.rostest
+def generate_test_description():
+    #todo change panda to a launch argument
+    moveit_config = MoveItConfigsBuilder("moveit_resources_panda").to_moveit_configs()
+
+    franka_kin_solver_node =  launch_ros.actions.Node(
+                package="dualq_kinematics",
+                executable="dualq_kinematics_FrankaKinSolverTest",
+                #Uncomment following line to debug with gdb
+                #prefix=['gdb -ex run --args'],
+                parameters=[
+                    moveit_config.robot_description,
+                    moveit_config.robot_description_semantic,
+                    moveit_config.robot_description_kinematics,
+
+                ],
+                output="screen",
+    )
+
+    return (
+        launch.LaunchDescription([
+            
+            #Node under test
+            franka_kin_solver_node,
+            KeepAliveProc(),
+            launch_testing.actions.ReadyToTest(),
+
+            #Launch tests 0.5s later
+            # launch.actions.TimerAction(
+            #      period=0.5, actions=[launch_testing.actions.ReadyToTest()]),
+            
+        ]),
+        {"franka_kin_solver_node": franka_kin_solver_node},
+    )
+
+
+class TestTerminatingProcessStops(unittest.TestCase):
+    def test_gtest_run_complete(self, proc_info, franka_kin_solver_node):
+        proc_info.assertWaitForShutdown(process=franka_kin_solver_node, timeout=40000.0)
+
+# Post-shutdown tests
+@launch_testing.post_shutdown_test()
+class FrankaKinSolverShutdown(unittest.TestCase):
+    def test_exit_codes(self, proc_info):
+        """Check if the processes exited normally."""
+        launch_testing.asserts.assertExitCodes(proc_info)
